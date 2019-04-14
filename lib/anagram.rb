142cbb7cb836f9f19c4fd5d6dcfd3aea89fa6989
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 00:53:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,20 @@
 # Your code goes here!
 
 class Anagram
+  @@all= []
 
   attr_accessor :word
 
   def initialize(word)
     @word = word
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.match
+    word.match(%w())
   end
 end
