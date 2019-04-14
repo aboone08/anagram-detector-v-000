582bfe37a67cd37c530cbb5c9bd158e3b99a2d83
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 02:18:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,17 +3,12 @@
 class Anagram
   attr_accessor :word
 
-  @@all = []
-
   def initialize(word)
     @word = word
-    @@all << self
-
   end
 
   def match(anagram)
-    anagram = word.split("").sort == @word.split("").sort
-    %w(@@all) 
+    %w(@word) 
 
   #  array.select do |word|
   #    word.split("").sort == @word.split("").sort
