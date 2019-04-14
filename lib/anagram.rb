3001faf4512fce5b08593b64a6650e3816d54940
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 02:36:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,9 +9,9 @@ class Anagram
 
   def match(word_array)
     word_array.select do |word|
-      @word.split("").sort
+      %w().split("").sort
       #word.split("").sort == @word.split("").sort
-      %w()
+      
     end
   end
 end
