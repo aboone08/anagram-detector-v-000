9bf33ce4aac4d95d273f91e56a8c4559cf546283
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 02:38:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
 
   def match(word_array)
     word_array.select do |word|
-      %w().split("").sort == @word.split("").sort
+      %w()
       #word.split("").sort == @word.split("").sort
 
     end
