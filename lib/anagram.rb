412bb1808928d1586480e42d185cf1957bdda81d
Andranique Boone <andranique.boone@gmail.com>
2019-04-14 02:36:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,9 +9,9 @@ class Anagram
 
   def match(word_array)
     word_array.select do |word|
-      word.split("").sort
+      @word.split("").sort
       #word.split("").sort == @word.split("").sort
-      %w(@word)
+      %w()
     end
   end
 end
